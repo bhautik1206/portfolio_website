246017bf6e5ad7066915093206827f73f97ef308
Bhautik <bhautikkapadiya06@gmail.com>
2025-11-10 17:10:21 +0530

    fix(index): adding index js in the file

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import { useState, useEffect } from "react";
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import { HashRouter as Router, Route, Switch } from "react-router-dom";
 import Contact from "./views/Contact";
 import Navbar from "./components/Navbar";
 import About from "./views/About";
